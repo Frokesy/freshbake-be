7665f800132890c8e87c4afbef155d7eb31b3902
Frokesy <akindeleayanfeoluwa@gmail.com>
2024-09-21 22:43:38 +0100

    project purpose changed from BE to admin dashboard

--- utils/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://irjvdbavljsoerfoizrm.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlyanZkYmF2bGpzb2VyZm9penJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ5NjQ4NzcsImV4cCI6MjA0MDU0MDg3N30.GoNCAEqB-TOZftxHoeAmQgHjmcfBjKieuOTgPeqBC-8'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
